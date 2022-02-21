e9f3e814368dbf558d0460bb78e5c133362f370a
Bojan Peric <bojan.peric@everon.io>
2022-02-21 14:29:11 +0100

    Initial commit

---
+++ Netflix Clone iOS15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone iOS15
+//
+//  Created by Bojan Peric on 21.2.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Netflix-Clone-iOS15
+ Netflix Clone app done using Swift5 and UIKit
