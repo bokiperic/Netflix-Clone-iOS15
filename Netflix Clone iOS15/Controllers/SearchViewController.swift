004a756123e6a8e5587c4a679ca3edfca046ecba
Bojan Peric <bojan.peric@everon.io>
2022-02-21 18:20:29 +0100

    Creating MainTabBarViewController

---
+++ Netflix Clone iOS15/Controllers/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone iOS15
+//
+//  Created by Bojan Peric on 21.2.22..
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
